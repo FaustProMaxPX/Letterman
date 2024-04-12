475f7d821339dd17d83cb9015fbdb6dd90cc03ea
Zephyr <1546327522@qq.com>
2024-04-12 22:01:03 +0800

    fix: repeat request

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -1,15 +1,20 @@
 import { useDispatch } from "react-redux";
 import { hideMessage, showMessage } from "../reducers/notificationReducer";
+import { useCallback } from "react";
 
 const useMessage = () => {
   const dispatch = useDispatch();
-  const openMessage = (content: string, duration = 3000) => {
-    const id = Math.random();
-    dispatch(showMessage({ id, content }));
-    setTimeout(() => {
-      dispatch(hideMessage(id));
-    }, duration);
-  };
+  const openMessage = useCallback(
+    (content: string, duration = 3000) => {
+      const id = Math.random();
+      dispatch(showMessage({ id, content }));
+      const timer = setTimeout(() => {
+        dispatch(hideMessage(id));
+      }, duration);
+      return () => clearTimeout(timer);
+    },
+    [dispatch]
+  );
   return openMessage;
 };
 
